2ad6282fa7ef321a9df5bb81bc67029b20117992
Hiram He <hiram.he@qq.com>
2019-12-23 14:58:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ rm -rf inst.R
 echo "[ ## 2.] Installing prodigal_modified"
 
 cd prodigal_modified
-make CC=$GCC # conda will add $GCC to environment
+make # conda will add $GCC to environment
 mv prodigal ../envs/FACS_env/bin/prodigal_sm
 
 source deactivate
