dfc74f7fa5ee9e6a3571152f3f5b656a45861d2c
Luis Pedro Coelho <luis@luispedro.org>
2020-01-01 05:32:19 +1100

    BLD Simplify conda package building

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-conda config --env --add channels defaults
-conda config --env --add channels r
-conda config --env --add channels bioconda
-conda config --env --add channels conda-forge
-
 outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 mkdir -p $PREFIX/bin
@@ -16,8 +11,6 @@ make
 chmod +x prodigal
 cd ..
 
-# save source code
-cp -r $SRC_DIR/example_seqs/ $outdir/example_seqs/
 cp $SRC_DIR/prodigal_modified/prodigal $outdir/envs/FACS_env/bin/prodigal_sm
 cp $SRC_DIR/CTDDClass.py $outdir/CTDDClass.py
 cp $SRC_DIR/FACS.sh $outdir/FACS.sh
@@ -28,12 +21,5 @@ cp $SRC_DIR/r22_largeTraining.rds $outdir/r22_largeTraining.rds
 cp $SRC_DIR/rf_dataset1.rds $outdir/rf_dataset1.rds
 
 
-# soft connection
 ln -s $outdir/* $PREFIX/bin/
 
-# start script
-#cp $RECIPE_DIR/FACS.py $outdir/FACS
-#chmod +x $outdir/FACS
-
-#soft connection
-#ln -s $outdir/FACS $PREFIX/bin
