42dfc237aed75dd7115769bb2865c1d675e150ce
Luis Pedro Coelho <luis@luispedro.org>
2020-01-12 04:04:42 +1100

    RFCT Implement Macrel API in Python
    Using flask, this becomes a very short programme

---
+++ website/api.py
@@ -0,0 +1,55 @@
+import flask
+from flask import request
+import subprocess
+import tempfile
+import pandas as pd
+from os import path
+import gzip
+
+app = flask.Flask('Macrel')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    args = ['macrel']
+    with tempfile.TemporaryDirectory() as tdir:
+        if request.form.get('dataType') == 'peptides':
+            args.append('peptides')
+            ifname = path.join(tdir, 'pep.faa')
+        else:
+            args.append('contigs')
+            ifname = path.join(tdir, 'contigs.fna')
+
+        if request.form.get('textData'):
+            with open(ifname, 'wt') as out:
+                out.write(request.form.get('textData'))
+        else:
+            ifname = path.join(tdir, request.files['file'].filename)
+            request.files['file'].save(ifname)
+
+        args.extend([
+            '--fasta', ifname,
+            '--output', tdir + '/macrel.out',
+            ])
+        subprocess.check_call(args)
+        tfile = path.join(tdir, 'macrel.out', 'macrel.out.prediction.gz')
+        data = pd.read_table(tfile)
+        data.rename(inplace=True, columns={
+            'Unnamed: 0': 'access',
+            'group': 'amp_family',})
+
+        r = flask.jsonify({
+            'code': 1,
+            'message': 'Macrel OK',
+            'rawdata': gzip.open(tfile, 'rt').read(),
+            'data': {
+                'objects': [
+                    data.iloc[i].to_dict()
+                        for i in range(len(data))]
+                },
+            })
+        r.headers.add('Access-Control-Allow-Origin', '*')
+        r.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        r.headers.add('Access-Control-Allow-Methods', 'GET,POST')
+        return r
+
+app.run()
