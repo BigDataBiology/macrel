f936553a8bd495e21448721cea0ed20ff7584913
Luis Pedro Coelho <luis@luispedro.org>
2020-01-14 15:52:27 +0800

    RFCT Remove repeated function from filter_smorfs

--- macrel/filter_smorfs.py
+++ macrel/filter_smorfs.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python
-
-# TODO This function is copy & paste from AMP-features.py, but should
-# eventually be merged
-def fasta_iter(fname):
-    header = None
-    chunks = []
-    with open(fname) as f:
-        for line in f:
-            if line[0] == '>':
-                if header is not None:
-                    yield header,''.join(chunks)
-                header = line[1:].strip().split()[0]
-                chunks = []
-            else:
-                chunks.append(line.strip())
-        if header is not None:
-            yield header, ''.join(chunks)
+from .fasta import fasta_iter
 
 def filter_smorfs(ifile, ofile):
     with open(ofile, 'wt') as output:
