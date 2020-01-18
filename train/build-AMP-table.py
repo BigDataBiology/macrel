f53808fc06836f68b03cb3ce55e90f1d26703454
Luis Pedro Coelho <luis@luispedro.org>
2020-01-18 13:56:06 +1100

    ENH Remove duplicated sequences from training set

--- train/build-AMP-table.py
+++ train/build-AMP-table.py
@@ -1,22 +1,29 @@
 from macrel.fasta import fasta_iter
 from macrel.AMP_features import features
 normalized_fname = 'data/AMP_NAMP.training.faa'
+
+# The AmPEP data has duplicates! The same exact same sequences appear on both
+# the positive and negative classes:
+seen = set()
 with open(normalized_fname, 'wt') as output:
     for i, (_, seq) in enumerate(fasta_iter('data/M_model_train_AMP_sequence.fasta')):
         output.write(f">AMP_{i}\n{seq}\n")
+        seen.add(seq)
     for i, (_, seq) in enumerate(fasta_iter('data/M_model_train_nonAMP_sequence.fasta')):
+        if seq in seen: continue
         output.write(f">NAMP_{i}\n{seq}\n")
+        seen.add(seq)
 fs = features(normalized_fname)
 fs['group'] = fs.index.map(lambda ix: ix.split('_')[0])
 fs.to_csv('data/AMP.train.tsv', sep='\t')
 
-normalized_fname_testing = 'data/AMP_NAMP.testing.faa'
-with open(normalized_fname_testing, 'wt') as output:
+normalized_fname_test = 'data/AMP_NAMP.test.faa'
+with open(normalized_fname_test, 'wt') as output:
     for i, (_, seq) in enumerate(fasta_iter('data/Supp-S2_AMP.faa')):
         output.write(f">AMP_{i}\n{seq}\n")
     for i, (_, seq) in enumerate(fasta_iter('data/Supp-S2_NAMP.faa')):
         output.write(f">NAMP_{i}\n{seq}\n")
         
-fs_v = features(normalized_fname_testing)
+fs_v = features(normalized_fname_test)
 fs_v['group'] = fs_v.index.map(lambda ix: ix.split('_')[0])
-fs_v.to_csv('data/AMP.testing.tsv', sep='\t')
+fs_v.to_csv('data/AMP.test.tsv', sep='\t')
