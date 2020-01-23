f229a8fe3c38abe96bbf16b0debbfaa61ad9d0d6
Luis Pedro Coelho <luis@luispedro.org>
2020-01-24 01:31:03 +1100

    TST Test on multiple Python versions

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# This is set in travis
+if [[ x$PYTHON_VERSION == x ]]; then
+    PYTHON_VERSION=3.7
+fi
+
 echo "
 # Macrel - (Meta)genomic AMP Classification and Retrieval
 
@@ -20,9 +25,10 @@ eval "$(conda shell.bash hook)"
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
 echo "# Creating new environment for Macrel"
 mkdir -p envs
-conda create --yes -p $BASEDIR/envs/Macrel_env python=3.7
+conda create --yes -p $BASEDIR/envs/Macrel_env python=$PYTHON_VERSION
 source activate $BASEDIR/envs/Macrel_env
 conda config --env --add channels defaults
 conda config --env --add channels bioconda
