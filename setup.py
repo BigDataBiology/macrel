d2fbbc41efa35293b2ac4fdf8b75dff7e1d00ac9
Luis Pedro Coelho <luis@luispedro.org>
2020-03-16 11:40:57 +0100

    MIN Python 3.8 is also supported

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ classifiers = [
 'Programming Language :: Python :: 3',
 'Programming Language :: Python :: 3.6',
 'Programming Language :: Python :: 3.7',
+'Programming Language :: Python :: 3.8',
 'Programming Language :: R',
 'Operating System :: OS Independent',
 'License :: OSI Approved :: MIT License',
