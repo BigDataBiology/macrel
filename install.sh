bb00f55370d3895dc9863844d9e557d48b4b171d
Celio Santos-Jr <celio.diasjunior@gmail.com>
2020-12-02 22:12:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "# Creating new environment for Macrel"
 mkdir -p envs
-conda create --yes -p $BASEDIR/envs/Macrel_env python=$PYTHON_VERSION r>=3.6,<4.0
+conda create --yes -p $BASEDIR/envs/Macrel_env python=$PYTHON_VERSION r=3.6 ## Fixed 
 source activate $BASEDIR/envs/Macrel_env
 conda config --env --add channels defaults
 conda config --env --add channels bioconda
