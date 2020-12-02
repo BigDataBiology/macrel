78f71ded1a0e6815e547f206a02758953eabc4c3
Celio Santos-Jr <celio.diasjunior@gmail.com>
2020-12-02 22:08:34 +0800

    Fix R version
    Fixing R version in the script to fit the R package peptides distribution

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ if [[ x$PYTHON_VERSION == x ]]; then
     PYTHON_VERSION=3.7
 fi
 
+if [[ x$R_VERSION == x ]]; then
+    R_VERSION>=3.6,<4.0
+fi
+
+
 echo "
 # Macrel - (Meta)genomic AMP Classification and Retrieval
 
@@ -28,7 +33,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "# Creating new environment for Macrel"
 mkdir -p envs
-conda create --yes -p $BASEDIR/envs/Macrel_env python=$PYTHON_VERSION
+conda create --yes -p $BASEDIR/envs/Macrel_env python=$PYTHON_VERSION r>=3.6,<4.0
 source activate $BASEDIR/envs/Macrel_env
 conda config --env --add channels defaults
 conda config --env --add channels bioconda
