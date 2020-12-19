01de959b8ec3555fb3a9cadbdc802a8d9e131498
psj1997 <1176894699@qq.com>
2020-12-19 13:30:49 +0800

    Fix failure of testing

--- tests/get-smorfs/command.sh
+++ tests/get-smorfs/command.sh
@@ -4,8 +4,8 @@ set -e
 
 mkdir -p out
 
-macrel get-smorfs -f excontigs.fna.gz --file-output - --log-file log1.txt > out/macrel.out.smorfs.faa
-macrel get-smorfs -f excontigs.fna.gz --file-output - --keep-fasta-headers --log-file >(gzip > log2.txt.gz) > out/macrel.out.smorfs.w_headers.faa
+macrel get-smorfs -f excontigs.fna.gz --file-output - --output out1 --log-file log1.txt > out/macrel.out.smorfs.faa
+macrel get-smorfs -f excontigs.fna.gz --file-output - --keep-fasta-headers --output out1 --log-file >(gzip > log2.txt.gz) > out/macrel.out.smorfs.w_headers.faa
 
 test -f log1.txt
 test -f log2.txt.gz
