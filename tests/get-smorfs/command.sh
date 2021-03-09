705bb662d95b06b78a6161d447a8caa39b6efa31
Celio Santos-Jr <celio.diasjunior@gmail.com>
2021-03-10 05:48:59 +0800

    fix log2.txt.gz to log2.txt

--- tests/get-smorfs/command.sh
+++ tests/get-smorfs/command.sh
@@ -5,8 +5,7 @@ set -e
 mkdir -p out
 
 macrel get-smorfs -f excontigs.fna.gz --file-output - --log-file log1.txt > out/macrel.out.smorfs.faa
-macrel get-smorfs -f excontigs.fna.gz --file-output - --keep-fasta-headers --log-file log2.txt.gz > out/macrel.out.smorfs.w_headers.faa
+macrel get-smorfs -f excontigs.fna.gz --file-output - --keep-fasta-headers --log-file log2.txt > out/macrel.out.smorfs.w_headers.faa
 
 test -f log1.txt
 test -f log2.txt
-
