88db5d054d5488a42d54739ffb6c533219b616f8
Celio Santos-Jr <celio.diasjunior@gmail.com>
2021-03-10 01:22:15 +0800

    add test for model lt50

--- macrel/tests/test_predict.py
+++ macrel/tests/test_predict.py
@@ -12,3 +12,14 @@ def test_predict():
                         fs, keep_negatives=True)
     assert len(fsp) < len(fsn)
     assert not np.all(fsn.is_AMP)
+
+def test_predict_lt50():
+    fs = AMP_features.features('tests/peptides/expep.faa.gz')
+    fsp = AMP_predict.predict( data_file("models/AMP_lt50.pkl.gz"),
+                        data_file("models/Hemo_lt50.pkl.gz"),
+                        fs)
+    fsn = AMP_predict.predict( data_file("models/AMP_lt50.pkl.gz"),
+                        data_file("models/Hemo_lt50.pkl.gz"),
+                        fs, keep_negatives=True)
+    assert len(fsp) < len(fsn)
+    assert not np.all(fsn.is_AMP)
