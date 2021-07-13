861ffdc062bc2eebcedfd784ed07e57b2e056e27
Luis Pedro Coelho <luis@luispedro.org>
2021-07-13 10:34:53 +0200

    BUG Fix for very short peptides
    Pad with sequence with 'X'. The result is kind of fake and we warn the
    user, but we no longer crash.
    
    close #40
    
    Backported from master branch

--- macrel/tests/test_predict.py
+++ macrel/tests/test_predict.py
@@ -12,3 +12,14 @@ def test_predict():
                         fs, keep_negatives=True)
     assert len(fsp) < len(fsn)
     assert not np.all(fsn.is_AMP)
+
+def test_predict_very_short():
+    fs = AMP_features.features(
+            path.join(path.dirname(__file__),
+                    'data',
+                    'very_short.faa'))
+    assert len(fs) == 2
+    fsn = AMP_predict.predict(data_file("models/AMP.pkl.gz"),
+                        data_file("models/Hemo.pkl.gz"),
+                        fs, keep_negatives=True)
+    assert not np.any(fsn.is_AMP)
