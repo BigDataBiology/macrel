ed2da19092fd5deef2dded2a0dba99018ea32f95
Celio Santos <celio.diasjunior@gmail.com>
2022-04-11 20:39:46 -0300

    ENH correct deprecated local pip install

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-$PYTHON -m pip install --disable-pip-version-check --no-cache-dir --ignore-installed --no-deps -vv .
+$PYTHON -m pip install --disable-pip-version-check --no-cache-dir --ignore-installed --no-deps --use-feature=in-tree-build -vv .
