56a92cf3b2dbe7e4ff25560ba37c9c7dcf59e767
Celio Santos-Jr <celio.diasjunior@gmail.com>
2022-04-12 10:20:40 -0300

    REF dry install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,7 @@ fi
 
 echo "
 # Macrel - (Meta)genomic AMP Classification and Retrieval
-
 AUTHORS: Célio Dias Santos Júnior, Luis Pedro Coelho
-
 "
 
 if ! which conda > /dev/null; then
@@ -30,52 +28,46 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "# Creating new environment for Macrel"
 mkdir -p envs
 conda create --yes -p $BASEDIR/envs/Macrel_env python=$PYTHON_VERSION
-
-source activate $BASEDIR/envs/Macrel_env
 conda config --env --add channels defaults
 conda config --env --add channels bioconda
 conda config --env --add channels conda-forge
 
 if ! which mamba > /dev/null; then
     CONDA_INSTALL_CMD=conda
-    conda install -y \
-              ngless \
-              megahit \
-              paladin \
-              pandas \
-              scikit-learn \
-              atomicwrites \
-              tzlocal \
-              pyrodigal>=0.7.3 \
-              --quiet
 else
     echo "# Installing packages with MAMBA"
     CONDA_INSTALL_CMD=mamba
-    mamba install -y \
-              ngless \
-              megahit \
-              paladin \
-              pandas \
-              scikit-learn \
-              atomicwrites \
-              rpy2 \
-              tzlocal \
-              pyrodigal>=0.7.3 \
-              --quiet
 fi
 
+${CONDA_INSTALL_CMD} install -y \
+          --prefix $BASEDIR/envs/Macrel_env \
+          ngless \
+          pyrodigal>=0.7.3 \
+          megahit \
+          paladin \
+          pandas \
+          scikit-learn \
+          atomicwrites \
+          tzlocal \
+          make \
+          c-compiler
+
+# only activate AFTER install all the packages to get the right environmental variables
+source activate $BASEDIR/envs/Macrel_env
+
+# env forwards the current environment and sets some extra variables needed by
+# ./recipe/build.sh
 env \
         PYTHON=$(which python) \
         SRC_DIR=$PWD \
         PREFIX=$CONDA_PREFIX \
         ./recipe/build.sh
-        
+
+
 echo "############ Installation procedures finished
 ****** Thank you for installing Macrel ********
 --- Please submit bugreports/comments to
 celio.diasjunior@gmail.com
-
 or
-
 https://github.com/BigDataBiology/macrel/issues
 "
